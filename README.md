54aaa0dd996d48d457440d8df16103dc47a9b183
Nathan Nowack <thrast36@gmail.com>
2023-12-30 16:12:50 -0600

    flow

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Today's Content
 
-foo
+```python
+from marvin import ai_fn
+
+@ai_fn
+def stereotypical_X_person_say(name: str, qualities: str) -> str:
+    """give dry / funny sterotypical quotes from {{ name }} given qualities {{ qualities }}"""
+
+stereotypical_X_person_say("Nate", "gen Z, Pisces, hipster")
+# => "Hey, it's Nate. Just chilling, listening to some lo-fi beats and drinking my oat milk latte. Thinking about the universe and stuff, you know? Pisces vibes."
+```
 
 # Archive
-- [2023-12-30_16-03-41](archive/2023-12-30_16-03-41.md)
---
+++ update_readme.py
@@ -0,0 +1,85 @@
+import subprocess
+from datetime import datetime
+from pathlib import Path
+
+from prefect import flow, task
+
+def archive_today_content(readme_lines: list[str], archive_dir: Path) -> str:
+    start = readme_lines.index('# Today\'s Content') + 2
+    end = readme_lines.index('# Archive') if '# Archive' in readme_lines else len(readme_lines)
+    today_content = '\n'.join(readme_lines[start:end]).strip()
+    
+    if not today_content:
+        return ""
+    
+    datetime_string = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    archive_file = archive_dir / f"{datetime_string}.md"
+    archive_file.write_text(today_content, encoding='utf-8')
+    return datetime_string
+
+def update_archive_section(readme_lines: list[str], datetime_string: str, archive_dir: Path) -> None:
+    archive_link = f"- [{datetime_string}]({archive_dir / datetime_string}.md)"
+    if '# Archive' not in readme_lines:
+        readme_lines.append('# Archive\n')
+    archive_idx = readme_lines.index('# Archive') + 1
+    readme_lines.insert(archive_idx, archive_link)
+
+@task
+def update_readme(new_content: str, readme_path: str = 'README.md', archive_dir_name: str = 'archive') -> None:
+    readme = Path(readme_path)
+    archive_dir = Path(archive_dir_name)
+    archive_dir.mkdir(parents=True, exist_ok=True)
+
+    readme_lines = readme.read_text(encoding='utf-8').splitlines()
+    datetime_string = archive_today_content(readme_lines, archive_dir)
+    
+    if datetime_string:
+        update_archive_section(readme_lines, datetime_string, archive_dir)
+    
+    today_idx = readme_lines.index('# Today\'s Content') + 2
+    archive_idx = readme_lines.index('# Archive')
+    readme_lines[today_idx:archive_idx] = [new_content.strip()] + ['']
+
+    readme.write_text('\n'.join(readme_lines) + '\n', encoding='utf-8')
+
+@task(task_run_name="Run command: `{command}`")
+def run_command(command: str):
+    process = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process.stdout.decode().strip()
+
+@flow
+def update_and_merge_readme(
+    new_content: str,
+    readme_path: str = 'README.md',
+    archive_dir_name: str = 'archive'
+):
+    if not new_content:
+        raise ValueError("New content cannot be empty")
+    
+    update_readme(new_content, readme_path, archive_dir_name)
+    
+    branch_name = f"content-update-{datetime.now().strftime('%Y%m%d%H%M%S')}"
+    
+    run_command(f"git checkout -b {branch_name}")
+    run_command(f"git add {readme_path} {archive_dir_name}")
+    run_command("git commit -m 'Update README with new content'")
+    run_command(f"git push origin {branch_name}")
+
+    pr_title = "Automated README update for {}".format(datetime.now().strftime("%Y-%m-%d"))
+    pr_body = "This is an automated update of the README content."
+    
+    run_command(f"gh pr create --title '{pr_title}' --body '{pr_body}' --base main --head {branch_name}")
+    run_command(f"gh pr merge {branch_name} --auto --squash")
+
+update_and_merge_readme.from_source(
+    source="https://github.com/zzstoatzz/alternatebuild.dev.git",
+    entrypoint="update_readme.py:update_and_merge_readme",
+).deploy(
+    name="Update and Merge README for `zzstoatzz/alternatebuild.dev`",
+    work_pool_name="managed",
+    job_variables={
+        "env": {
+            "GITHUB_TOKEN": "{{ prefect.blocks.secret.github-token }}"
+        }
+    }
+)
