1649e959103c9540c64e03a8730ba6257d356e97
zzstoatzz <thrast36@gmail.com>
2024-10-25 02:41:59 -0500

    heeel yeah

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,12 @@ module.exports = {
   theme: {
     extend: {
       keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0) rotate(90deg)' },
-          '50%': { transform: 'translateY(-4px) rotate(90deg)' },
-        },
         'bounce-subtle': {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(-4px)' }
         }
       },
       animation: {
-        'float-arrows': 'float 2s ease-in-out infinite',
         'bounce-subtle': 'bounce-subtle 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       }
     }
